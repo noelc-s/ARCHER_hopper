beb2acf1eb8346f7fb844a4cae52b02b4b999c31
Noel Csomay-Shanklin <noelcs@caltech.edu>
2024-02-13 16:36:31 -0800

    got ethernet communication working

--- HardwareStack/src/EthernetTeensy/EthernetTeensy.ino
+++ HardwareStack/src/EthernetTeensy/EthernetTeensy.ino
@@ -24,6 +24,7 @@ bool read_packet = false;
 // static void printPrompt();
 void receivePacket();
 static void sendPacket();
+unsigned long t1, t2;
 
 // Program setup.
 void setup() {
@@ -70,6 +71,8 @@ void setup() {
 
   // Start UDP listening on the port
   udp.begin(kPort);
+
+  t1 = micros();
 
   // printPrompt();
 }
@@ -126,34 +129,34 @@ void receivePacket() {
   if (size < 0) {
     return;
   };
-
   // Get the packet data and remote address
   const uint8_t *data = udp.data();
   IPAddress ip = udp.remoteIP();
 
-  printf("[%u.%u.%u.%u][%d] ", ip[0], ip[1], ip[2], ip[3], size);
+  //printf("[%u.%u.%u.%u][%d] ", ip[0], ip[1], ip[2], ip[3], size);
 
   // Print each character
-  for (int i = 0; i < size; i++) {
-    uint8_t b = data[i];
-    if (b < 0x20) {
-      printf("<%s>", kCtrlNames[b].c_str());
-    } else if (b < 0x7f) {
-      putchar(data[i]);
-    } else {
-      printf("<%02xh>", data[i]);
-    }
-  }
-  printf("\r\n");
+  // for (int i = 0; i < size; i++) {
+  //   uint8_t b = data[i];
+  //   if (b < 0x20) {
+  //     printf("<%s>", kCtrlNames[b].c_str());
+  //   } else if (b < 0x7f) {
+  //     putchar(data[i]);
+  //   } else {
+  //     printf("<%02xh>", data[i]);
+  //   }
+  // }
+  // printf("\r\n");
   read_packet = true;
-  printf("Set read_packet to true\n");
+  t2 = micros();
+  // printf("Set read_packet to true\n");
 }
 
 static void sendPacket() {
   static String line;
-  line = "Dane Joe";
+  line = "Dane Joe Note that micro-benchmarking is hard. An accurate timer is only a small part of what's necessary to get meaningful results for short timed regions. See Idiomatic way of performance evaluation? for some more general caveats)";
 
-  IPAddress ip = Ethernet.broadcastIP();
+  //IPAddress ip = Ethernet.broadcastIP();
   // printf("    Broadcast IP     = %u.%u.%u.%u\r\n", ip[0], ip[1], ip[2], ip[3]);  
   // printf(Ethernet.broadcastIP());
   
@@ -162,196 +165,15 @@ static void sendPacket() {
     // printf("Attempting to send.");
   if (read_packet) {
     // printf("Attempting to send.\n");
-    if (!udp.send(Ethernet.broadcastIP(), kPort,
+    IPAddress ip_send(10,0,0,6);
+    if (!udp.send(ip_send, kPort,
                   reinterpret_cast<const uint8_t *>(line.c_str()),
                   line.length())) {
       // printf("[Error sending]\r\n");
     }
-    line = "";
+    //line = "";
+    read_packet = false;
+    Serial.println(t2-t1);
+    t1 = t2;
   }
-  read_packet = false;
 }
-
-// // SPDX-FileCopyrightText: (c) 2023 Shawn Silverman <shawn@pobox.com>
-// // SPDX-License-Identifier: AGPL-3.0-or-later
-
-// // BroadcastChat is a simple chat application that broadcasts and
-// // receives text messages over UDP.
-// //
-// // This file is part of the QNEthernet library.
-
-// #include <QNEthernet.h>
-
-// using namespace qindesign::network;
-
-// // --------------------------------------------------------------------------
-// //  Configuration
-// // --------------------------------------------------------------------------
-
-// constexpr uint16_t kPort = 8080;  // Chat port
-
-// // --------------------------------------------------------------------------
-// //  Program State
-// // --------------------------------------------------------------------------
-
-// // UDP port.
-// EthernetUDP udp;
-
-// // --------------------------------------------------------------------------
-// //  Main Program
-// // --------------------------------------------------------------------------
-
-// // Forward declarations (not really needed in the Arduino environment)
-// static void printPrompt();
-// static void receivePacket();
-// static void sendLine();
-
-// // Program setup.
-// void setup() {
-//   Serial.begin(115200);
-//   while (!Serial && millis() < 4000) {
-//     // Wait for Serial
-//   }
-//   printf("Starting...\r\n");
-
-//   uint8_t mac[6];
-//   Ethernet.macAddress(mac);  // This is informative; it retrieves, not sets
-//   printf("MAC = %02x:%02x:%02x:%02x:%02x:%02x\r\n",
-//          mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-
-//   Ethernet.onLinkState([](bool state) {
-//     printf("[Ethernet] Link %s\r\n", state ? "ON" : "OFF");
-//   });
-
-//   IPAddress ip(10, 0, 0, 7);
-//   IPAddress sn{255,255,255,0};  // Subnet Mask
-//   IPAddress gw{10,0,0,1};       // Default Gateway
-
-//   printf("Starting Ethernet with DHCP...\r\n");
-//   if (!Ethernet.begin(ip, sn, gw)) {
-//     printf("Failed to start Ethernet\r\n");
-//     return;
-//   }
-
-//   // if (!Ethernet.waitForLocalIP(kDHCPTimeout)) {
-//   //   printf("Failed to get IP address from DHCP\r\n");
-//   //   return;
-//   // }
-//   printf("Obtaining the IP, Subnet and Gateway\r\n");
-//   ip = Ethernet.localIP();
-//   printf("    Local IP     = %u.%u.%u.%u\r\n", ip[0], ip[1], ip[2], ip[3]);
-//   ip = Ethernet.subnetMask();
-//   printf("    Subnet mask  = %u.%u.%u.%u\r\n", ip[0], ip[1], ip[2], ip[3]);
-//   ip = Ethernet.broadcastIP();
-//   printf("    Broadcast IP = %u.%u.%u.%u\r\n", ip[0], ip[1], ip[2], ip[3]);
-//   ip = Ethernet.gatewayIP();
-//   printf("    Gateway      = %u.%u.%u.%u\r\n", ip[0], ip[1], ip[2], ip[3]);
-//   ip = Ethernet.dnsServerIP();
-//   printf("    DNS          = %u.%u.%u.%u\r\n", ip[0], ip[1], ip[2], ip[3]);
-
-//   // Start UDP listening on the port
-//   udp.begin(kPort);
-
-//   printPrompt();
-// }
-
-// // Main program loop.
-// void loop() {
-//   // printf("Recieving Packet..\r\n");
-//   receivePacket();
-//   // printf("Sending Packet.. \r\n");
-//   sendLine();
-// }
-
-// // --------------------------------------------------------------------------
-// //  Internal Functions
-// // --------------------------------------------------------------------------
-
-// // Control character names.
-// static const String kCtrlNames[]{
-//   "NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL",
-//   "BS",  "HT",  "LF",  "VT",  "FF",  "CR",  "SO",  "SI",
-//   "DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB",
-//   "CAN", "EM",  "SUB", "ESC", "FS",  "GS",  "RS",  "US",
-// };
-
-// // Receives and prints chat packets.
-// static void receivePacket() {
-//   int size = udp.parsePacket();
-//   if (size < 0) {
-//     return;
-//   }
-
-//   // Get the packet data and remote address
-//   const uint8_t *data = udp.data();
-//   IPAddress ip = udp.remoteIP();
-
-//   printf("[%u.%u.%u.%u][%d] ", ip[0], ip[1], ip[2], ip[3], size);
-
-//   // Print each character
-//   for (int i = 0; i < size; i++) {
-//     uint8_t b = data[i];
-//     if (b < 0x20) {
-//       printf("<%s>", kCtrlNames[b].c_str());
-//     } else if (b < 0x7f) {
-//       putchar(data[i]);
-//     } else {
-//       printf("<%02xh>", data[i]);
-//     }
-//   }
-//   printf("\r\n");
-// }
-
-// // Tries to read a line from the console and returns whether
-// // a complete line was read. This is CR/CRLF/LF EOL-aware.
-// static bool readLine(String &line) {
-//   static bool inCR = false;  // Keeps track of CR state
-
-//   while (Serial.available() > 0) {
-//     int c;
-//     switch (c = Serial.read()) {
-//       case '\r':
-//         inCR = true;
-//         return true;
-
-//       case '\n':
-//         if (inCR) {
-//           // Ignore the LF
-//           inCR = false;
-//           break;
-//         }
-//         return true;
-
-//       default:
-//         if (c < 0) {
-//           return false;
-//         }
-//         inCR = false;
-//         line.append(static_cast<char>(c));
-//     }
-//   }
-
-//   return false;
-// }
-
-// // Prints the chat prompt.
-// static void printPrompt() {
-//   printf("chat> ");
-//   fflush(stdout);  // printf may be line-buffered, so ensure there's output
-// }
-
-// // Reads from the console and sends packets.
-// static void sendLine() {
-//   static String line;
-
-//   // Read from the console and send lines
-//   if (readLine(line)) {
-//     if (!udp.send(Ethernet.broadcastIP(), kPort,
-//                   reinterpret_cast<const uint8_t *>(line.c_str()),
-//                   line.length())) {
-//       printf("[Error sending]\r\n");
-//     }
-//     line = "";
-//     printPrompt();
-//   }
-// }
--- HardwareStack/src/EthernetTest.cpp
+++ HardwareStack/src/EthernetTest.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 #include <string>
 
 #include <arpa/inet.h> // htons, inet_addr
@@ -9,39 +10,64 @@
 
 int main(int argc, char const *argv[])
 {
-    std::string hostname{"10.0.0.7"};
+    std::string hostname{"10.0.0.6"};
+    std::string destname{"10.0.0.7"};
     uint16_t port = 4333;
 
     int sock = ::socket(AF_INET, SOCK_DGRAM, 0);
 
-    sockaddr_in destination;
+    sockaddr_in source, destination;
+    source.sin_family = AF_INET;
+    source.sin_port = htons(port);
+    source.sin_addr.s_addr = inet_addr(hostname.c_str());
+
     destination.sin_family = AF_INET;
     destination.sin_port = htons(port);
-    destination.sin_addr.s_addr = inet_addr(hostname.c_str());
+    destination.sin_addr.s_addr = inet_addr(destname.c_str());
 
-    std::string msg = "Jane Doe";
-    int n_bytes = ::sendto(sock, msg.c_str(), msg.length(), 0, reinterpret_cast<sockaddr*>(&destination), sizeof(destination));
-    std::cout << n_bytes << " bytes sent" << std::endl;
-    
+    int enabled = 1;
+    setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &enabled, sizeof(enabled));
+
+    int val = bind(sock, (struct sockaddr*)&source,
+             sizeof(source));
+    std::cout << val << std::endl;
+    perror("bind");
+    printf("Error binding socket: %d\n", errno);
+
+    std::string msg = "So, if you'd like to simplify it down and make it more C-like, including removing all of the type-safe class stuff it does, here are 3 simple and very easy-to-use functions to get timestamps in milliseconds, microseconds, and nanoseconds...that only took me about 12 hrs to write*:";
+
    char buffer[1024];
     sockaddr_in senderAddr;
+    senderAddr.sin_family = AF_INET;
+    senderAddr.sin_port = htons(port);
+    senderAddr.sin_addr.s_addr = inet_addr(hostname.c_str());
     socklen_t senderAddrLen = sizeof(senderAddr);
 
     ssize_t recvBytes = 0;
     // do {
-        recvBytes = ::recvfrom(sock, buffer, sizeof(buffer), 0, reinterpret_cast<sockaddr*>(&senderAddr), &senderAddrLen);
-        std::cout << recvBytes << std::endl;
+        //recvBytes = ::recvfrom(sock, buffer, sizeof(buffer), 0, reinterpret_cast<sockaddr*>(&senderAddr), &senderAddrLen);
+	//std::cout << recvBytes << std::endl;
     // } while(recvBytes==0);
 
-    // ssize_t recvBytes = ::recvfrom(sock, buffer, sizeof(buffer), 0, reinterpret_cast<sockaddr*>(&senderAddr), &senderAddrLen);
-    if (recvBytes > 0) {
-        std::cout<<"YEP"<<std::endl;
-        buffer[recvBytes] = '\0'; // Null-terminate the received data
-        std::cout << "Received: " << buffer << std::endl;
-    } else if (recvBytes == 0) {
-        std::cerr << "Connection closed by peer\n";
-    } else {
-        std::cerr << "Error receiving data\n";
+    int n_bytes = 0;
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    auto t2 = std::chrono::high_resolution_clock::now();
+    long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(
+        t2 - t1).count();
+
+    while(1) {
+    n_bytes = ::sendto(sock, msg.c_str(), msg.length(), 0, reinterpret_cast<sockaddr*>(&destination), sizeof(destination));
+    //std::cout << n_bytes << " bytes sent" << std::endl;
+    t2 = std::chrono::high_resolution_clock::now(); 
+    microseconds = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
+    std::cout << microseconds<< std::endl;
+    recvBytes = ::recvfrom(sock, buffer, sizeof(buffer), 0, reinterpret_cast<sockaddr*>(&senderAddr), &senderAddrLen);
+    std::cout << "Received: " << buffer << std::endl;
+    t2 = std::chrono::high_resolution_clock::now(); 
+    microseconds = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
+    std::cout << microseconds<< std::endl;
+    t1 = t2;
     }
 
     ::close(sock);
